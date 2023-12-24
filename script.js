d70cbde49c31a2a2c333a6493087a3ce1cfaf957
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-25 00:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ function fibonacci(num) {
 	}else if(num===1){
 		return 1;
 	}else{
-		return fibonacci(num-1)+fibonacci(num-2);
+		let fibArr=[0,1];
+		for(let i=2;i<=num;i++){
+			fibArr[i]=fibArr[i-1]+fibArr[i-2];
+		}
+	}
+	 return fibArr[num];
 	}
 }
 
